f83da93f181d417ad3c42dfb7df5021d0dee5219
Devmachado <deividkevin735@gmail.com>
2025-03-31 15:39:18 -0300

    a

--- render-build.sh
+++ render-build.sh
@@ -6,8 +6,11 @@ apt-get update
 # Instala o espeak e suas dependências
 apt-get install -y espeak libespeak1 libespeak-dev
 
-# Instala a biblioteca libespeak necessária para o pyttsx3
+# Garante que a biblioteca libespeak esteja instalada
 apt-get install -y libespeak1
 
 # Instala as dependências do Python
 pip install -r requirements.txt
+
+# Verifica se a biblioteca libespeak está acessível
+ldconfig
