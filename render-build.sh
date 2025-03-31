c7c82b0e8896d95c56c2d723eff8ba9b13a73343
Devmachado <deividkevin735@gmail.com>
2025-03-31 14:58:53 -0300

    a

--- render-build.sh
+++ render-build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-# Atualiza os pacotes e instala o espeak
-apt-get update && apt-get install -y espeak
+# Atualiza os pacotes do sistema
+apt-get update
+
+# Instala o espeak e suas dependências
+apt-get install -y espeak libespeak1
 
 # Instala as dependências do Python
 pip install -r requirements.txt
